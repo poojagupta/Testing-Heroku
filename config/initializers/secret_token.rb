35b37ae80d264f493cd3b026313a8025741554be
Pooja Gupta <gupta21pooja@gmail.com>
2011-07-14 13:00:10 +0530

    Added application on git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calculator::Application.config.secret_token = '2a56e918f33a9c085cd5732be8b303ef45a2f9d3714805009b5dd328397923dbd3688ed7907a5507abca49c5440446245622feb3d16fcc39190cd4f611de129a'
